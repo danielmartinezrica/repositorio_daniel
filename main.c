5d27814ef2f7a07b8dad2085bf6f1e4449abe02a
Daniel Martínez <dmr536@alumnos.unican.es>
2021-04-28 15:08:02 +0200

    En la rama experimentos, he añadido un segundo saludo

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 int main() {
    
-   printf("Hola, este es el programa de Dani");
+   printf("Hola, este es el programa de Dani\n");
+   printf("Hola otra vez);
    return 0;
 }
